c8b77a6fde9045a11aa167fc5348abe39e4a40be
acheron <acheron@pop-os.localdomain>
2019-11-17 14:00:00 +0100

    Better everything

--- src/App.vue
+++ src/App.vue
@@ -10,19 +10,35 @@
     <transition name="topbar-animation" enter-active-class="fade-in" leave-active-class="fade-out">
       <router-view></router-view>
     </transition>
+    <transition name="topbar-animation" enter-active-class="fade-in" leave-active-class="fade-out">
+      <FooterBar></FooterBar>
+    </transition>
   </div>
 </template>
 
 <script>
 import TopNavbar from "./components/common/TopNavbar";
+import FooterBar from "./components/common/FooterBar";
 
 export default {
   name: "app",
-  components: { TopNavbar },
+  components: { TopNavbar, FooterBar },
   watch: {
     $route: function() {
       this.isTopNavbarVisible = this.$route.path !== "/";
     }
+  },
+  methods: {
+    onScroll: function() {
+      let scrollTop = document.documentElement.scrollTop;
+      this.isTopNavbarVisible = scrollTop < 100;
+    }
+  },
+  mounted() {
+    document.addEventListener("scroll", this.onScroll);
+  },
+  beforeDestroy() {
+    document.removeEventListener("scroll", this.onScroll);
   },
   data: function() {
     return {
---
+++ src/components/common/FooterBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer>
+    <nav>
+      <ul>
+        <li>
+          <a class="primary-link" href="mailto:bartekbrozek@gmail.com">Created by Bartosz Brożek</a>
+        </li>
+      </ul>
+    </nav>
+  </footer>
+</template>
